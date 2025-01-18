e2a8a4cc9f2475f55a1400bd70b673bab2a77a45
akihiko.kigure <akihiko.kigure@gmail.com>
2025-01-18 16:12:30 +0900

    feat: add struct for main

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,6 +1,16 @@
 use actix_web::{web, App, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize)]
+struct InputValue {
+    value: i32,
+}
+
+#[derive(Serialize)]
+struct OutputValue {
+    result: i32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
