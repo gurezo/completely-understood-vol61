d2155ab27b850261432a1b0ee81b0ff50f7747d2
akihiko.kigure <akihiko.kigure@gmail.com>
2025-01-18 16:13:06 +0900

    feat: add method double_value for main

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -11,6 +11,11 @@ struct OutputValue {
     result: i32,
 }
 
+async fn double_value(input: web::Json<InputValue>) -> impl Responder {
+    let result = input.value * 2;
+    web::Json(OutputValue { result })
+}
+
 fn main() {
     println!("Hello, world!");
 }
